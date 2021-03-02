c3198d562efc9b2a6dc912f3d87836adb213550a
Cosmin Crisan <cosmin.v.crisan@gmail.com>
2021-03-02 22:43:43 +0200

    More code refactoring
    Added all the methods that manage array data in the manageArray class

--- src/interfaces/ArrayManager.java
+++ src/interfaces/ArrayManager.java
@@ -1,41 +1,82 @@
 /**
- * 
+ *
  */
 package interfaces;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * @author dan.nicoara
- * 
+ *
  * Creates arrays and manages array data. (TODO - SOLVED?)
  */
 public interface ArrayManager {
-	/**
-	 * generate an array containing all letters for a number of alphabetMultiplier times
-	 * 
-	 * @param alphabetMultiplier - the number of times each letter is repeated in the array (TODO - SOLVED)
-	 */
-	public void setCharArray(int alphabetMultiplier);
+    /**
+     *
+     */
+    void manageEvaluationData();
 
-	/**
-	 * shuffle the array
-	 *
-	 * @param charArray
-	 */
-	 public void shuffleCharArray(char[] charArray);
+    /**
+     *
+     */
+    void managePracticeData();
 
-	public void manageEvaluationData();
+    /**
+     * generate an array containing all letters for a number of alphabetMultiplier times
+     *
+     * @param alphabetMultiplier - the number of times each letter is repeated in the array (TODO - SOLVED)
+     */
+    void setCharArray(int alphabetMultiplier);
 
-	/**
-	 * create a new charMap and ads the chars to the map
-	 */
-	 public void setNewCharMap();
+    /**
+     * shuffle the array
+     *
+     * @param charArray - the array that needs to be shuffled
+     */
+    void shuffleCharArray(char[] charArray);
 
-	public char[] getShuffledArray();
+    /**
+     * create a new charMap and add the chars to the map
+     */
+    void setNewCharMap();
 
-	public LinkedHashMap<String, Integer> getCharMap();
+    /**
+     * return the shuffled array
+     */
+    char[] getShuffledArray();
 
-	public LinkedHashMap<String, Integer> getDividerMap();
+    /**
+     * calculates the average time for each char
+     */
+    void calculateAverage();
+
+    /**
+     * sort the charMap in ascending order
+     */
+    void sortCharMap();
+
+    /**
+     * transfer the sorted chars to a char array
+     */
+    void setCharArrayFromSortedMap();
+
+    /**
+     * generate an array with characters repeating proportionally to the reaction
+     * time for each char
+     */
+    void setPracticeArray();
+
+    /**
+     * calculate how many times a char was deployed and store the result in a hashmap
+     *
+     * @param typedChar - the char that was received as input in the last keystroke
+     */
+    void addDividerToMap(char typedChar);
+
+    /**
+     * add the reaction time for the typed char
+     *
+     * @param elapsedTime - the time that elapsed between
+     * @param typedChar - the char that was received as input in the last keystroke
+     */
+    void addTimeToMap(int elapsedTime, char typedChar);
+
 }
--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -10,58 +10,28 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class StartUp {
+    private static ArrayManager manageArray;
+    private final TextField textField = new TextField(10);
+    // generate a start and end Instant for calculating the elapsed time
+    Instant start = Instant.now(), end = Instant.now();
     // the frame
     private Frame mainFrame;
     // label for displaying the letters
     private Label headerLabel;
-    // label for displaying additional information
-    private Label statusLabel;
     // add panel
     private Panel controlPanel;
-    // array for storing the letters needed in the evaluation phase
-    private char[] evaluationArray;
-    // array for transferring the chars from the sorted map and displaying them on
-    private char[] charArrayFromSortedMap;
-    // array to store the shuffled letters
-    private char[] shuffledArray;
-    // array for storing the letters needed in the practice phase
-    private char[] practiceArray;
     // int for accessing the index of an array
     private int charArrayIndex = 0;
-    // int for calculating how many times a letter is displayed in the evaluation
-    // sequence
-    private int alphabetMultiplier = 2;
-    // the number of letters in the alphabet
-    private final int numberOfLetters = 26;
-    private final TextField textField = new TextField(10);
-    // generate a start and end Instant for calculating the elapsed time
-    Instant start = Instant.now(), end = Instant.now();
-    // create a string to store the chars when needed as string
-    private String charString;
-    // current char
-    private char currentChar;
-    // empty hash map for storing the alphabet and adding the reaction time for each
-    // character
-    private LinkedHashMap<String, Integer> charMap = new LinkedHashMap<>();
-    // hash map for storing and recording the sorted chars
-    private LinkedHashMap<String, Integer> sortedMap;
-    // hash map for storing the number of times a char is displayed (needed for
-    // calculating the average)
-    private LinkedHashMap<String, Integer> dividerMap = new LinkedHashMap<>();
     // char for storing the current typed char
     private char typedChar;
     // int for storing the elapsed time
-    private int time;
-    private static ArrayManager manageArray;
+    private int elapsedTime;
 
     // evaluate the typing speed for each character
     public StartUp(ArrayManager manageArray) {
-        this.manageArray = manageArray;
+        StartUp.manageArray = manageArray;
         prepareGUI();
         manageArray.manageEvaluationData();
         runEvaluation();
@@ -75,7 +45,7 @@ public class StartUp {
 
     // prepare the GUI
     private void prepareGUI() {
-        mainFrame = new Frame("Java AWT Examples");
+        mainFrame = new Frame("Typing Practice");
         mainFrame.setSize(400, 400);
         mainFrame.setLayout(new GridLayout(3, 1));
         mainFrame.addWindowListener(new WindowAdapter() {
@@ -86,7 +56,8 @@ public class StartUp {
         headerLabel = new Label();
         headerLabel.setAlignment(Label.CENTER);
         headerLabel.setFont(new Font("Serif", Font.PLAIN, 43));
-        statusLabel = new Label();
+        // label for displaying additional information
+        Label statusLabel = new Label();
         statusLabel.setAlignment(Label.CENTER);
         statusLabel.setSize(350, 100);
 
@@ -99,91 +70,15 @@ public class StartUp {
         mainFrame.setVisible(true);
     }
 
-    /*
-     * generate an array with characters repeating proportionally to the reaction
-     * time for each char
-     */
-    private void setPracticeArray(char[] array) {
-
-        // calculate the size of the practiceArray
-        int sizeOfPracticeArray = numberOfLetters * (numberOfLetters + 1) / 2;
-        // create the array
-        practiceArray = new char[sizeOfPracticeArray];
-        // set index
-        int index = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                practiceArray[index] = array[i];
-                index++;
-            }
-        }
-    }
-
-    // sort the charMap in ascending order
-    private void sortCharMap() {
-        // generate a new, empty sortedMap
-        sortedMap = new LinkedHashMap<>();
-        // arrayList for storing and sorting the chars
-        ArrayList<Integer> charList = new ArrayList<>();
-        // transfer the chars from charMap to charList
-        for (Map.Entry<String, Integer> entry : charMap.entrySet()) {
-            charList.add(entry.getValue());
-        }
-        // rank the chars by typing speed (fastest to slowest)
-        Collections.sort(charList);
-        // transfer the ranked list to sortedMap
-        for (int num : charList) {
-            for (Entry<String, Integer> entry : charMap.entrySet()) {
-                if (entry.getValue().equals(num)) {
-                    sortedMap.put(entry.getKey(), num);
-                }
-            }
-        }
-    }
-
-    // transfer the sorted chars to a char array
-    private void setCharArrayFromSortedMap() {
-        charArrayFromSortedMap = sortedMap.keySet().stream().map(String::valueOf).collect(Collectors.joining())
-                .toCharArray();
-
-    }
 
     // calculates the reaction time
     private void calculateElapsedTime(Instant start, Instant end) {
         this.start = start;
         this.end = end;
         Duration timeElapsed = Duration.between(start, end);
-        time = (int) timeElapsed.toMillis();
+        elapsedTime = (int) timeElapsed.toMillis();
     }
 
-    // ads the reaction time for the typed char
-    private void addTimeToMap(int time) {
-        this.time = time;
-        charString = Character.toString(typedChar);
-        int sum = manageArray.getCharMap().get(charString) + time;
-        charMap.put(charString, sum);
-    }
-
-    // how many times the time was added for the typed char
-    private void addDividerToMap() {
-        int divider = manageArray.getDividerMap().get(charString);
-        charString = Character.toString(typedChar);
-        dividerMap.put(charString, divider + 1);
-    }
-
-    // calculates the average time for each char
-    private void calculateAverage() {
-        currentChar = 'a';
-        int average;
-
-        for (int i = 0; i < numberOfLetters; i++) {
-            charString = Character.toString(currentChar);
-            average = charMap.get(charString) / dividerMap.get(charString);
-            charMap.put(charString, average);
-            currentChar++;
-        }
-    }
 
     private void runProgram(char[] array) {
 
@@ -195,11 +90,11 @@ public class StartUp {
                 typedChar = e.getKeyChar();
                 end = Instant.now();
 
-                if (typedChar == array[charArrayIndex] && charArrayIndex < array.length) {
+                if (typedChar == array[charArrayIndex]) {
 
                     calculateElapsedTime(start, end);
-                    addTimeToMap(time);
-                    addDividerToMap();
+                    manageArray.addTimeToMap(elapsedTime, typedChar);
+                    manageArray.addDividerToMap(typedChar);
                     charArrayIndex++;
 
                     if (charArrayIndex >= array.length) {
@@ -230,13 +125,7 @@ public class StartUp {
     }
 
     private void runPractice() {
-        calculateAverage();
-        sortCharMap();
-        setCharArrayFromSortedMap();
-        // reset the charMap to store new data
-        manageArray.setNewCharMap();
-        setPracticeArray(charArrayFromSortedMap);
-        manageArray.shuffleCharArray(practiceArray);
-        runProgram(shuffledArray);
+        manageArray.managePracticeData();
+        runProgram(manageArray.getShuffledArray());
     }
 }
