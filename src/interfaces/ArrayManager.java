c9f6b0f190114281d8b2d91dd2f612c8e018a6b8
Dan Nicoara <dan.nicoara@3pillarglobal.com>
2021-03-03 09:34:32 +0200

    Revert "Changed the signature of a few methods inside ArrayManager/ManagerArray."
    This reverts commit ea3382ea48337fa2aaf29acd083249135061fc54.

--- src/interfaces/ArrayManager.java
+++ src/interfaces/ArrayManager.java
@@ -20,9 +20,49 @@ public interface ArrayManager {
     void managePracticeData();
 
     /**
+     * generate an array containing all letters for a number of alphabetMultiplier times
+     *
+     * @param alphabetMultiplier - the number of times each letter is repeated in the array (TODO - SOLVED)
+     */
+    void setCharArray(int alphabetMultiplier);
+
+    /**
+     * shuffle the array
+     *
+     * @param charArray - the array that needs to be shuffled
+     */
+    void shuffleCharArray(char[] charArray);
+
+    /**
+     * create a new charMap and add the chars to the map
+     */
+    void setNewCharMap();
+
+    /**
      * return the shuffled array
      */
     char[] getShuffledArray();
+
+    /**
+     * calculates the average time for each char
+     */
+    void calculateAverage();
+
+    /**
+     * sort the charMap in ascending order
+     */
+    void sortCharMap();
+
+    /**
+     * transfer the sorted chars to a char array
+     */
+    void setCharArrayFromSortedMap();
+
+    /**
+     * generate an array with characters repeating proportionally to the reaction
+     * time for each char
+     */
+    void setPracticeArray();
 
     /**
      * calculate how many times a char was deployed and store the result in a hashmap
