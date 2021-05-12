b6f323ebb9563c0b8e04145f98bc60564359fc10
Cosmin-Crisan <cosmin.v.crisan@gmail.com>
2021-05-12 23:30:03 +0300

    Update ManageMap.java
    Started implementing encapsulation by using one map

--- src/bussinessLogic/ManageMap.java
+++ src/bussinessLogic/ManageMap.java
@@ -17,31 +17,32 @@
 
 package bussinessLogic;
 
+import interfaces.MapManager;
+import models.LetterAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import interfaces.MapManager;
-
 public class ManageMap implements MapManager {
 
     // empty hash map for storing the alphabet and adding the reaction time for each
     // character
-    LinkedHashMap<String, Integer> charMap = new LinkedHashMap<>();
+    // LinkedHashMap<String, Integer> charMap = new LinkedHashMap<>();
     // hash map for storing the number of times a char is displayed (needed for
     // calculating the average)
     LinkedHashMap<String, Integer> dividerMap = new LinkedHashMap<>();
     LinkedHashMap<String, Integer> sortedMap;
-    
+
     // TODO - use this instead of charMap, dividerMap and sortedMap.
-    //LinkedHashMap<String, LetterAttributes> map = new LinkedHashMap<>();
+    LinkedHashMap<String, LetterAttributes> map = new LinkedHashMap<>();
+    LetterAttributes la =  (LetterAttributes)map.get("a");
     /**
-     *  
+     *
      *  map.put("a", new LetterAttributes(0, 0));
     	map.put("b", new LetterAttributes(0, 0));
     	map.put("c", new LetterAttributes(0, 0));
-    	
+
     	LetterAttributes la =  (LetterAttributes)map.get("a");
      */
 
@@ -55,7 +56,8 @@ public class ManageMap implements MapManager {
 
         for (int i = 0; i < Constants.NUMBER_OF_LETTERS; i++) {
             charString = Character.toString(currentChar);
-            charMap.put(charString, 0);
+            // charMap.put(charString, 0);
+            map.put(charString, new LetterAttributes(0, 0));
             dividerMap.put(charString, 0);
             currentChar++;
         }
@@ -94,8 +96,9 @@ public class ManageMap implements MapManager {
 
     public void addDividerToMap(char typedChar) {
     	String typedCharacter = Character.toString(typedChar);
-        int divider = dividerMap.get(typedCharacter);
-        dividerMap.put(typedCharacter, divider + 1);
+        LetterAttributes la =  (LetterAttributes)map.get(typedCharacter);
+        int divider = la.getCount();
+        la.setCount(divider + 1);
     }
 
     /**
@@ -106,8 +109,9 @@ public class ManageMap implements MapManager {
      */
     public void addTimeToMap(int elapsedTime, char typedChar) {
         String typedCharacter = Character.toString(typedChar);
-        int sum = charMap.get(typedCharacter) + elapsedTime;
-        charMap.put(typedCharacter, sum);
+        LetterAttributes la =  (LetterAttributes)map.get(typedCharacter);
+        int sum = la.getTime() + elapsedTime;
+        la.setTime(sum);
     }
 
     /**
@@ -116,12 +120,13 @@ public class ManageMap implements MapManager {
     public void calculateAverage() {
     	String charString;
         char currentChar = 'a';
-        int average;
+        int averageTime;
 
         for (int i = 0; i < Constants.NUMBER_OF_LETTERS; i++) {
             charString = Character.toString(currentChar);
-            average = charMap.get(charString) / dividerMap.get(charString);
-            charMap.put(charString, average);
+            LetterAttributes la =  (LetterAttributes)map.get(charString);
+            averageTime = la.getTime() / la.getCount();
+            la.setTime(averageTime);
             currentChar++;
         }
     }
